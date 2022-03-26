4a618771eb4285d3e63dd551a71513d13b0dbbc9
Daniil <daniil.molokov13@gmail.com>
2022-03-26 22:57:19 +0300

    Работа с объектами

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,15 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    if (data.gender == 'female') {
+        delete data.age;
+    }
+    if (data.gender == 'male') {
+        if (!data.hasOwnProperty('income')) {
+            data.income = 100000;
+        }
+    }
+    return data;
 }
 
 /*
@@ -15,6 +24,17 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    let data = [];
+    for (let key in obj1) {
+        data.push(key);
+    }
+    for (let key in obj2) {
+        data.push(key);
+    }
+    for (let key in obj3) {
+        data.push(key);
+    }
+    return data.sort();
 }
 
 /*
@@ -23,4 +43,11 @@ export function objectFieldsList(obj1, obj2, obj3) {
   Количество клонов - count.
 */
 export function objectClone(obj, count) {
+    let data = [];
+    for (let i = 0; i < count; i++) {
+        let clone = JSON.parse(JSON.stringify(obj));
+        clone.id = i;
+        data.push(clone);
+    }
+    return data;
 }
